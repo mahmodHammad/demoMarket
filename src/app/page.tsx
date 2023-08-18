f985ac13f8945114e573ca8acd81b13c2a72eeb3
Mahmoud Hammad <hodaman2012@gmail.com>
2023-08-18 18:27:07 +0300

    test ci

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
       <div>
-        <div>Mahmoud Hammad</div>
-        <div>Javascript developer</div>
-        <div>Egypy</div>
-
+       
       </div>
   )
 }
