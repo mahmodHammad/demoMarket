5c59e105f7cb799e64084e383bd5311f3f6a3dbe
Mahmoud Hammad <hodaman2012@gmail.com>
2023-08-20 01:50:35 +0300

    App layout done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,13 @@
 import * as React from "react";
-import Link from "next/link";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import ThemeRegistry from "@/ThemeRegistry/ThemeRegistry";
-import { Button } from "@/common";
+import { Box } from "@/common";
+import { Footer, Navbar } from "@/component";
 
 export const metadata = {
-  title: "Next.js App Router + Material UI v5",
-  description: "Next.js App Router + Material UI v5",
+  title: "Atar Market place",
+  description:
+    "Property Portal Explore Our Wide Range of Properties for Sale and Rentals in Saudi arabia, Golf, and middle east and more Verified Listings: Apartments, Villas, Houses Find Your Perfect Appartment their",
 };
-
-const DRAWER_WIDTH = 240;
 
 export default function RootLayout({
   children,
@@ -23,37 +18,17 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <ThemeRegistry>
-          <AppBar position="fixed" sx={{ zIndex: 2000 }}>
-            <Toolbar
-              sx={{
-                backgroundColor: "background.primary",
-                justifyContent: "space-between",
-              }}
-            >
-              <Typography variant="h6" noWrap component="div" color="white">
-                Logo
-              </Typography>
-
-              <Box>
-                <Button sx={{ color: "#fff" }} size="large">
-                  Login
-                </Button>
-                <Button size="large" variant="outlined" whiteborder>
-                  Signup
-                </Button>
-              </Box>
-            </Toolbar>
-          </AppBar>
-
+          <Navbar />
           <Box
             component="main"
             sx={{
               pt: "80px",
-              pl:"20px"
+              pl: "20px",
             }}
           >
             {children}
           </Box>
+          <Footer />
         </ThemeRegistry>
       </body>
     </html>
