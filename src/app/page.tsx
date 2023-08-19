f6c7854b4f0d4b85f0fa2f4f8201945c2e2168dc
Mahmoud Hammad <hodaman2012@gmail.com>
2023-08-20 02:55:08 +0300

    Implement hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,12 @@ import styles from "./page.module.css";
 import Link from "next/link";
 import { Box, Button } from "@mui/material";
 import { Text } from "@/common";
+import { Hero } from "@/component";
 
 export default function Home() {
   return (
     <Box>
-     
+      <Hero/>
     </Box>
   );
 }
---
+++ src/component/homepage/Hero.tsx
@@ -0,0 +1,73 @@
+import { Box, Text } from "@/common";
+import React from "react";
+import Image from "next/image";
+import heroBG from "@/assets/images/herobg.png";
+export default function Hero() {
+  return (
+    <Box
+      sx={{
+        height: { xs: "calc(100vh - 220px)", md: "calc(100vh - 100px)" },
+        position: "relative",
+      }}
+    >
+      <Box
+        sx={{
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          objectPosition: "bottom",
+          position: "absolute",
+        }}
+        component={Image}
+        priority
+        alt="houses and properties for rent"
+        src={heroBG}
+        quality={1}
+        placeholder="blur"
+      />
+
+      <Box
+        sx={{
+          position: "absolute",
+          zIndex: 100,
+          width: "100%",
+          height: "100%",
+        }}
+        center
+        column
+      >
+        <Box sx={{ maxWidth: { xl: "720px", md: "60%", xs: "90%" } }}>
+          <Text variant="h2" align="center" sx={{ color: "#fff" }}>
+            Buy, rent, or sell your property easily
+          </Text>
+          <Text
+            variant="body"
+            align="center"
+            sx={{
+              mt: { md: "26px", xs: "16px" },
+              color: "#fff",
+              textAlign: "center",
+            }}
+          >
+            Lorem ipsum dolor sit amet consectetur. Neque augue dictum nullam
+            sollicitudin morbi mauris ornare bibendum condimentum.Lorem ipsum
+            dolor sit amet consectetur. Neque augue dictum nullam sollicitudin
+            morbi mauris ornare bibendum condimentum.
+          </Text>
+        </Box>
+
+        <Box
+          sx={{
+            mt: { md: "100px", xs: "50px" },
+            width: { xs: "90%", lg: "900px" },
+            height: "102px",
+            borderRadius: "20px 20px 20px 20px",
+            background: "rgba(255, 255, 255, 0.70)",
+            boxShadow: "0px 30px 60px -15px rgba(143, 144, 188, 0.15)",
+            backdropFilter: "blur(10px)",
+          }}
+        ></Box>
+      </Box>
+    </Box>
+  );
+}
