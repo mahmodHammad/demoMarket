fc0007653c4cd6975c80a4ae0d4311bfa95d473c
Mahmoud Hammad <hodaman2012@gmail.com>
2023-08-21 03:56:29 +0300

    neighbourhood card

---
+++ src/component/cards/NeighbourhoodCard.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Box, Text } from "@/wrappers";
+import Image from "next/image";
+
+interface proptypes {
+  img: stirng;
+  title: string;
+  link: string;
+}
+export default function NeighbourhoodCard({ img, title, link }: proptypes) {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: { xs: "157px", md: "273px" },
+        position: "relative",
+        overflow: "hidden",
+        borderRadius: "16px",
+      }}
+    >
+      <Box
+        sx={{
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          objectPosition: "bottom",
+          position: "absolute",
+        }}
+        component={Image}
+        alt="houses and properties for rent"
+        src={img}
+      />
+      <Text
+        variant="h5"
+        sx={{
+          color: "#fff",
+          bottom: "20px",
+          left: "20px",
+          position:"absolute"
+        }}
+      >
+        {title}
+      </Text>
+    </Box>
+  );
+}
