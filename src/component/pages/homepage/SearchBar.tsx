54dc05e9598aaa955862f4d95465029dcfcb5f7d
abawazir <abawazir@goatar.com>
2023-09-06 05:07:24 +0300

    mobile

--- src/component/pages/homepage/SearchBar.tsx
+++ src/component/pages/homepage/SearchBar.tsx
@@ -4,20 +4,72 @@ import React, { useState } from "react";
 import {
   Autocomplete,
   ButtonBase,
+  Checkbox,
   Divider,
+  FormControl,
   IconButton,
+  InputLabel,
+  ListItemText,
+  MenuItem,
+  OutlinedInput,
+  Select,
+  SelectChangeEvent,
+  Slider,
   TextField,
 } from "@mui/material";
-
 import { Location, Search } from "@/assets";
 
+function valuetext(value: number) {
+  return `${value}°C`;
+}
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const names = [
+  "Residential Apartment",
+  "Residential Land",
+  "Independent House/Villa",
+];
+
 const SearchBar = () => {
+  const [age, setAge] = React.useState("");
+
+  const handleChangee = (event: SelectChangeEvent) => {
+    setAge(event.target.value);
+  };
+
+  const [values, setValues] = React.useState<number[]>([20, 37]);
+
+  const handleChanges = (event: Event, newValue: number | number[]) => {
+    setValues(newValue as number[]);
+  };
+  const [personName, setPersonName] = React.useState<string[]>([]);
+
+  const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === "string" ? value.split(",") : value
+    );
+  };
+
   const defaultProps = {
-    options: top100Films,
+    options: Locations,
     getOptionLabel: (option: FilmOptionType) => option.title,
   };
   const flatProps = {
-    options: top100Films.map((option) => option.title),
+    options: Locations.map((option) => option.title),
   };
   const [value, setValue] = React.useState<FilmOptionType | null>(null);
 
@@ -31,7 +83,6 @@ const SearchBar = () => {
     setIsRent(true);
   };
 
-  // const [selected , setSelected] = useState{ture};
   return (
     <Box sx={{ mt: { xs: "20px ", md: "60px" } }}>
       <Box row>
@@ -43,7 +94,10 @@ const SearchBar = () => {
               height: { xs: "20px", md: "39px" },
 
               backgroundColor: isRent ? null : "#FFFFFF",
-              borderRadius: "15px 15px 0px 0px",
+              borderRadius: {
+                xs: "8px 8px 0px 0px",
+                md: "15px 15px 0px 0px",
+              },
             }}
           >
             <Text variant="small" sx={{ color: isRent ? "#FFFFFF" : null }}>
@@ -59,7 +113,10 @@ const SearchBar = () => {
               width: { xs: "40px", md: "102px" },
               height: { xs: "20px", md: "39px" },
               backgroundColor: isRent ? "#FFFFFF" : null,
-              borderRadius: "15px 15px 0px 0px",
+              borderRadius: {
+                xs: "8px 8px 0px 0px",
+                md: "15px 15px 0px 0px",
+              },
             }}
           >
             <Text variant="small" sx={{ color: isRent ? null : "#FFFFFF" }}>
@@ -70,102 +127,240 @@ const SearchBar = () => {
       </Box>
       <Box
         xbetween
+        row
         sx={{
           width: { xs: "370px", md: "918px" },
           height: { xs: "60px", md: "102px" },
-
           top: "347px",
           borderRadius: " 0px 20px 20px 20px",
           boxShadow: " 0px 30px 60px -15px #8F90BC26",
           backgroundColor: "#FFFFFFCC",
-
           backdropFilter: "blur(10px)",
-          p: "24px",
+          p: { xs: "5px", md: "24px" },
         }}
       >
-        <Box row yend sx={{ display: { xs: "none", md: "flex" } }}>
-          <Box column width={"183px"} mr={"30px"}>
-            <Text variant="label">Location</Text>
-            <Autocomplete
-              {...defaultProps}
-              id="disable-close-on-select"
-              disableCloseOnSelect
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  variant="standard"
-                  placeholder="Select Your City"
-                />
-              )}
-            />
+        <Box xbetween width={"100%"}>
+          <FormControl sx={{ minWidth: "100px" }} size="small">
+            <InputLabel id="demo-simple-select-autowidth-label">
+              Location
+            </InputLabel>
+            <Select
+              sx={{
+                "& fieldset": { border: "none" },
+              }}
+              labelId="demo-simple-select-autowidth-label"
+              id="demo-simple-select-autowidth"
+              value={age}
+              onChange={handleChange}
+              autoWidth
+              label="Age"
+              placeholder="kk"
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={10}>Twenty</MenuItem>
+              <MenuItem value={21}>Twenty one</MenuItem>
+              <MenuItem value={22}>Twenty one and a half</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl sx={{ minWidth: "100px" }} size="small">
+            <InputLabel id="demo-simple-select-autowidth-label">
+              Property Type
+            </InputLabel>
+            <Select
+              sx={{
+                "& fieldset": { border: "none" },
+              }}
+              labelId="demo-simple-select-autowidth-label"
+              id="demo-simple-select-autowidth"
+              value={age}
+              onChange={handleChange}
+              autoWidth
+              label="Age"
+              placeholder="kk"
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={10}>Twenty</MenuItem>
+              <MenuItem value={21}>Twenty one</MenuItem>
+              <MenuItem value={22}>Twenty one and a half</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl sx={{ minWidth: "100px" }} size="small">
+            <InputLabel id="demo-simple-select-autowidth-label">
+              Price Range
+            </InputLabel>
+            <Select
+              sx={{
+                "& fieldset": { border: "none" },
+              }}
+              labelId="demo-simple-select-autowidth-label"
+              id="demo-simple-select-autowidth"
+              value={age}
+              onChange={handleChange}
+              autoWidth
+              label="Age"
+              placeholder="kk"
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={10}>Twenty</MenuItem>
+              <MenuItem value={21}>Twenty one</MenuItem>
+              <MenuItem value={22}>Twenty one and a half</MenuItem>
+            </Select>
+          </FormControl>
+
+          <Box
+            row
+            yend
+            width={"100%"}
+            sx={{ display: { xs: "none", md: "flex" } }}
+          >
+            <Box column mr={"30px"} width={"100%"}>
+              <Text variant="label">Location</Text>
+              <Autocomplete
+                popupIcon={null}
+                {...defaultProps}
+                id="disable-close-on-select"
+                disableCloseOnSelect
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    // InputProps={{
+                    //   disableUnderline: true,
+                    // }}
+                    variant="standard"
+                    placeholder="Select Your City"
+                  />
+                )}
+              />
+            </Box>
+            <Location />
+            <Box center ml={"10px"} mr={"35px"}>
+              <Divider
+                orientation="vertical"
+                sx={{
+                  width: "2px",
+                  background: "rgba(255, 66, 66, 0.08)",
+                  border: "0px",
+                  height: "46px",
+                }}
+              />
+            </Box>
           </Box>
-          <Location />
+          <Box
+            row
+            yend
+            width={"100%"}
+            sx={{ display: { xs: "none", md: "flex" } }}
+          >
+            <Box column mr={"20px"} width={"100%"}>
+              <Text variant="label">Property Type</Text>
+
+              <Select
+                sx={{
+                  "& fieldset": { border: "none" },
+                  height: "32px",
+                }}
+                labelId="demo-multiple-checkbox-label"
+                placeholder="Choose Property Type"
+                id="demo-multiple-checkbox"
+                displayEmpty
+                multiple
+                value={personName}
+                onChange={handleChange}
+                input={<OutlinedInput />}
+                renderValue={(selected) => {
+                  if (selected.length === 0) {
+                    return <Text>Choose Property Type</Text>;
+                  }
+
+                  return selected.join(", ");
+                }}
+                MenuProps={MenuProps}
+              >
+                {names.map((name) => (
+                  <MenuItem key={name} value={name}>
+                    <Checkbox checked={personName.indexOf(name) > -1} />
+                    <ListItemText primary={name} />
+                  </MenuItem>
+                ))}
+              </Select>
+
+              {/* <Autocomplete
+                {...defaultProps}
+                id="disable-close-on-select"
+                disableCloseOnSelect
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    variant="standard"
+                    placeholder="Choose Property Type "
+                    InputProps={{
+                      disableUnderline: true,
+                    }}
+                  />
+                )}
+              /> */}
+            </Box>{" "}
+            <Box center mr={"35px"}>
+              <Divider
+                orientation="vertical"
+                sx={{
+                  width: "2px",
+                  background: "rgba(255, 66, 66, 0.08)",
+                  border: "0px",
+                  height: "46px",
+                }}
+              />
+            </Box>
+          </Box>
+          <Box
+            row
+            yend
+            width={"100%"}
+            sx={{ display: { xs: "none", md: "flex" } }}
+          >
+            <Box column width={"100%"} pr={"70px"}>
+              <Text variant="label">Price Range</Text>
+              <Autocomplete
+                popupIcon={null}
+                {...defaultProps}
+                id="disable-close-on-select"
+                disableCloseOnSelect
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    // InputProps={{
+                    //   disableUnderline: true,
+                    // }}
+                    variant="standard"
+                    placeholder="Choose Price Range"
+                  />
+                )}
+              />
+              {/* <Box sx={{ width: 100 }}>
+                <Slider
+                  getAriaLabel={() => "Temperature range"}
+                  value={values}
+                  onChange={handleChanges}
+                  valueLabelDisplay="auto"
+                  getAriaValueText={valuetext}
+                />
+              </Box> */}
+            </Box>
+          </Box>
         </Box>
 
-        <Box center>
-          <Divider
-            orientation="vertical"
-            sx={{
-              width: "2px",
-              background: "rgba(255, 66, 66, 0.08)",
-              border: "0px",
-              height: "46px",
-            }}
-          />
-        </Box>
-
-        <Box row yend sx={{ display: { xs: "none", md: "flex" } }}>
-          <Box column width={"183px"} mr={"30px"}>
-            <Text variant="label">Property Type</Text>
-            <Autocomplete
-              {...defaultProps}
-              id="disable-close-on-select"
-              disableCloseOnSelect
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  variant="standard"
-                  placeholder="Choose Property Type "
-                />
-              )}
-            />
-          </Box>
-        </Box>
-        <Box center>
-          <Divider
-            orientation="vertical"
-            sx={{
-              width: "2px",
-              background: "rgba(255, 66, 66, 0.08)",
-              border: "0px",
-              height: "46px",
-            }}
-          />
-        </Box>
-        <Box row yend sx={{ display: { xs: "none", md: "flex" } }}>
-          <Box column width={"183px"} mr={"30px"}>
-            <Text variant="label">Price Range</Text>
-            <Autocomplete
-              {...defaultProps}
-              id="disable-close-on-select"
-              disableCloseOnSelect
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  variant="standard"
-                  placeholder="Choose Property Type "
-                />
-              )}
-            />
-          </Box>
-        </Box>
         <Box
-          height={"54px"}
-          width={"54px"}
+          width={{ md: "54px" }}
+          height={{ md: "54px" }}
           bgcolor={"#008EA5"}
           borderRadius={"20px"}
           center
-          sx={{ display: { xs: "none", md: "flex" } }}
         >
           <IconButton aria-label="delete" size="large">
             <Search />
@@ -183,131 +378,8 @@ interface FilmOptionType {
   year: number;
 }
 
-// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
-const top100Films = [
-  { title: "The Shawshank Redemption", year: 1994 },
-  { title: "The Godfather", year: 1972 },
-  { title: "The Godfather: Part II", year: 1974 },
-  { title: "The Dark Knight", year: 2008 },
-  { title: "12 Angry Men", year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-  { title: "Pulp Fiction", year: 1994 },
-  {
-    title: "The Lord of the Rings: The Return of the King",
-    year: 2003,
-  },
-  { title: "The Good, the Bad and the Ugly", year: 1966 },
-  { title: "Fight Club", year: 1999 },
-  {
-    title: "The Lord of the Rings: The Fellowship of the Ring",
-    year: 2001,
-  },
-  {
-    title: "Star Wars: Episode V - The Empire Strikes Back",
-    year: 1980,
-  },
-  { title: "Forrest Gump", year: 1994 },
-  { title: "Inception", year: 2010 },
-  {
-    title: "The Lord of the Rings: The Two Towers",
-    year: 2002,
-  },
-  { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
-  { title: "Goodfellas", year: 1990 },
-  { title: "The Matrix", year: 1999 },
-  { title: "Seven Samurai", year: 1954 },
-  {
-    title: "Star Wars: Episode IV - A New Hope",
-    year: 1977,
-  },
-  { title: "City of God", year: 2002 },
-  { title: "Se7en", year: 1995 },
-  { title: "The Silence of the Lambs", year: 1991 },
-  { title: "It's a Wonderful Life", year: 1946 },
-  { title: "Life Is Beautiful", year: 1997 },
-  { title: "The Usual Suspects", year: 1995 },
-  { title: "Léon: The Professional", year: 1994 },
-  { title: "Spirited Away", year: 2001 },
-  { title: "Saving Private Ryan", year: 1998 },
-  { title: "Once Upon a Time in the West", year: 1968 },
-  { title: "American History X", year: 1998 },
-  { title: "Interstellar", year: 2014 },
-  { title: "Casablanca", year: 1942 },
-  { title: "City Lights", year: 1931 },
-  { title: "Psycho", year: 1960 },
-  { title: "The Green Mile", year: 1999 },
-  { title: "The Intouchables", year: 2011 },
-  { title: "Modern Times", year: 1936 },
-  { title: "Raiders of the Lost Ark", year: 1981 },
-  { title: "Rear Window", year: 1954 },
-  { title: "The Pianist", year: 2002 },
-  { title: "The Departed", year: 2006 },
-  { title: "Terminator 2: Judgment Day", year: 1991 },
-  { title: "Back to the Future", year: 1985 },
-  { title: "Whiplash", year: 2014 },
-  { title: "Gladiator", year: 2000 },
-  { title: "Memento", year: 2000 },
-  { title: "The Prestige", year: 2006 },
-  { title: "The Lion King", year: 1994 },
-  { title: "Apocalypse Now", year: 1979 },
-  { title: "Alien", year: 1979 },
-  { title: "Sunset Boulevard", year: 1950 },
-  {
-    title:
-      "Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb",
-    year: 1964,
-  },
-  { title: "The Great Dictator", year: 1940 },
-  { title: "Cinema Paradiso", year: 1988 },
-  { title: "The Lives of Others", year: 2006 },
-  { title: "Grave of the Fireflies", year: 1988 },
-  { title: "Paths of Glory", year: 1957 },
-  { title: "Django Unchained", year: 2012 },
-  { title: "The Shining", year: 1980 },
-  { title: "WALL·E", year: 2008 },
-  { title: "American Beauty", year: 1999 },
-  { title: "The Dark Knight Rises", year: 2012 },
-  { title: "Princess Mononoke", year: 1997 },
-  { title: "Aliens", year: 1986 },
-  { title: "Oldboy", year: 2003 },
-  { title: "Once Upon a Time in America", year: 1984 },
-  { title: "Witness for the Prosecution", year: 1957 },
-  { title: "Das Boot", year: 1981 },
-  { title: "Citizen Kane", year: 1941 },
-  { title: "North by Northwest", year: 1959 },
-  { title: "Vertigo", year: 1958 },
-  {
-    title: "Star Wars: Episode VI - Return of the Jedi",
-    year: 1983,
-  },
-  { title: "Reservoir Dogs", year: 1992 },
-  { title: "Braveheart", year: 1995 },
-  { title: "M", year: 1931 },
-  { title: "Requiem for a Dream", year: 2000 },
-  { title: "Amélie", year: 2001 },
-  { title: "A Clockwork Orange", year: 1971 },
-  { title: "Like Stars on Earth", year: 2007 },
-  { title: "Taxi Driver", year: 1976 },
-  { title: "Lawrence of Arabia", year: 1962 },
-  { title: "Double Indemnity", year: 1944 },
-  {
-    title: "Eternal Sunshine of the Spotless Mind",
-    year: 2004,
-  },
-  { title: "Amadeus", year: 1984 },
-  { title: "To Kill a Mockingbird", year: 1962 },
-  { title: "Toy Story 3", year: 2010 },
-  { title: "Logan", year: 2017 },
-  { title: "Full Metal Jacket", year: 1987 },
-  { title: "Dangal", year: 2016 },
-  { title: "The Sting", year: 1973 },
-  { title: "2001: A Space Odyssey", year: 1968 },
-  { title: "Singin' in the Rain", year: 1952 },
-  { title: "Toy Story", year: 1995 },
-  { title: "Bicycle Thieves", year: 1948 },
-  { title: "The Kid", year: 1921 },
-  { title: "Inglourious Basterds", year: 2009 },
-  { title: "Snatch", year: 2000 },
-  { title: "3 Idiots", year: 2009 },
-  { title: "Monty Python and the Holy Grail", year: 1975 },
+const Locations = [
+  { title: "Ryiadh", year: 1994 },
+  { title: "Khobar", year: 1972 },
+  { title: "Mecca", year: 1974 },
 ];
