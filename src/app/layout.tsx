d92ff406d40e81c2a176631bcbe952365ed040c3
Abdulrahman Mamdouh <abdulrahman.mamdouh@meng-tech.com>
2023-09-10 13:08:37 +0300

    fix build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,4 @@
-// 'use client';
-
-import React, { useState } from 'react';
+import * as React from "react";
 import ThemeRegistry from '@/ThemeRegistry/ThemeRegistry';
 import { Box } from '@/wrappers';
 import { Footer, Navbar, LoginModal } from '@/component';
