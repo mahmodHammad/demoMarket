ec44ca706e28bad38dac21b3fa776efa400410f8
Abdulrahman Mamdouh <abdulrahman.mamdouh@meng-tech.com>
2023-09-12 13:03:28 +0300

    seperate between onChange and onChangeCommitted events to prevent the run time error of the slider and make the mun value is 0

--- src/component/filters/SliderFilter.tsx
+++ src/component/filters/SliderFilter.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+'user client';
+
+import * as React from 'react';
 import { TextInput } from '@/component';
 import { Slider } from '@mui/material';
 import { Box } from '@/wrappers';
@@ -6,35 +8,52 @@ import { Box } from '@/wrappers';
 type Props = {
   label: string;
   sliderValues: number[];
-  handleSliderChange: (event: Event | null, newValue: number | number[]) => void;
+  handleSliderChange: (newValue: number[]) => void;
 };
 
 const SliderFilter = ({ label, sliderValues, handleSliderChange }: Props) => {
+  const [value, setValue] = React.useState<number[]>(sliderValues);
+
+  const handleChange = (event: Event | null, newValue: number[] | number) => {
+    setValue(newValue as number[]);
+  };
+
   return (
     <Box mt={5} column gap={'12px'} ycenter>
       <Slider
+        disableSwap
         size="medium"
         sx={{ width: 0.91 }}
-        value={sliderValues}
-        valueLabelDisplay="on"
+        value={value}
         getAriaLabel={() => `${label} range`}
-        onChange={handleSliderChange}
+        onChange={handleChange}
+        onChangeCommitted={(event: React.SyntheticEvent | Event, value: number | number[]) =>
+          handleSliderChange(value as number[])
+        }
+        valueLabelDisplay="auto"
       />
+
       <Box row xbetween ycenter gap={'18px'} sx={{ width: '100%' }}>
         <TextInput
           type="number"
-          value={sliderValues[0]}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-            handleSliderChange(null, [+event.target.value, sliderValues[1]])
-          }
+          value={value[0]}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            const inputValue = +event.target.value;
+            const minValue = 0;
+            const newValue = inputValue < minValue ? minValue : inputValue;
+            handleChange(null, [newValue, value[1]]);
+          }}
           placeholder={`Min ${label}`}
         />
         <TextInput
           type="number"
-          value={sliderValues[1]}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-            handleSliderChange(null, [sliderValues[0], +event.target.value])
-          }
+          value={value[1]}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            const inputValue = +event.target.value;
+            const minValue = 0;
+            const newValue = inputValue < minValue ? minValue : inputValue;
+            handleChange(null, [value[0], newValue]);
+          }}
           placeholder={`Max ${label}`}
         />
       </Box>
