03a8379592579fbdc76a3484f70594a0ed97c347
Abdulrahman Mamdouh <abdulrahman.mamdouh@meng-tech.com>
2023-09-13 13:45:18 +0300

    Implement slide effect to the filters

--- src/app/listingpage/page.tsx
+++ src/app/listingpage/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
-import { Grid, Container } from '@mui/material';
+import { Grid, Container, Slide } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { ListingBody, PropertyFilters } from '@/component';
@@ -19,18 +19,37 @@ export default function page() {
   return (
     <Container maxWidth="xl" sx={{ position: 'relative' }}>
       <Grid container spacing={3} sx={{ mt: '5px', pt: '26px' }} mb={15}>
-        <Grid
-          item
-          xs={12}
-          md={4}
-          display={{ md: 'flex', xs: showFiltersOnMob ? 'flex' : 'none', height: 'fit-content' }}>
-          <PropertyFilters isMobileView={isMobileView} closeFilterOnMobileView={() => setShowFiltersOnMob(false)} />
-        </Grid>
+        <SlideTransitions isMobileView={isMobileView} showFiltersOnMob={showFiltersOnMob}>
+          <Grid item xs={12} md={4} display={{ xs: 'flex', height: 'fit-content' }}>
+            <PropertyFilters isMobileView={isMobileView} closeFilterOnMobileView={() => setShowFiltersOnMob(false)} />
+          </Grid>
+        </SlideTransitions>
 
-        <Grid item xs={12} md={8} display={{ md: 'flex', xs: showFiltersOnMob ? 'none' : 'flex' }}>
+        <Grid item xs={12} md={8} display={{ xs: showFiltersOnMob ? 'none' : 'flex', md: 'flex' }}>
           <ListingBody isMobileView={isMobileView} openFilterOnMobileView={() => setShowFiltersOnMob(true)} />
         </Grid>
       </Grid>
     </Container>
   );
 }
+
+type SlideProps = {
+  isMobileView: boolean;
+  showFiltersOnMob: boolean;
+  children: any;
+};
+
+const SlideTransitions = ({ children, isMobileView, showFiltersOnMob }: SlideProps) => {
+  if (!isMobileView) return <>{children}</>;
+
+  return (
+    <Slide
+      direction="down"
+      in={isMobileView && showFiltersOnMob}
+      mountOnEnter
+      unmountOnExit
+      timeout={{ enter: 500, exit: 500 }}>
+      {children}
+    </Slide>
+  );
+};
