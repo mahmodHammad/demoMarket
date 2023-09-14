441afd7bf59c3c6ff0bacb148cf464610a81af97
abawazir <abawazir@goatar.com>
2023-09-14 17:45:03 +0300

    booking details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,18 @@
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "./page.module.css";
-import Link from "next/link";
-import { Box, Button, Container } from "@mui/material";
-import { Text } from "@/wrappers";
-import { Hero, Neighbourhoods } from "@/component";
-import RecentlyAdded from "@/component/pages/homepage/RecentlyAdded";
-import MostViewed from "@/component/pages/homepage/MostViewed";
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import styles from './page.module.css';
+import Link from 'next/link';
+import { Box, Button, Container } from '@mui/material';
+import { Text } from '@/wrappers';
+import { BookingDetails, Hero, Neighbourhoods } from '@/component';
+import RecentlyAdded from '@/component/pages/homepage/RecentlyAdded';
+import MostViewed from '@/component/pages/homepage/MostViewed';
 
 export default function Home() {
   return (
     <Box>
       {/* Logged In component */}
-        <div style={{}}>
-          hello
-          hello
-          hello
-          hello
-          hello
-          hello
-          hello
-          hello
-          hello
-          hello
-        </div>
-
+      <BookingDetails />
       {/* Logged out component */}
       {/* <Hero />
       <Container maxWidth="xl" sx={{ pb: { xs: "90px", md: "164px" } }}>
--- src/component/unitDetails/QuiltedImageList.tsx
+++ src/component/unitDetails/QuiltedImageList.tsx
@@ -1,13 +1,15 @@
-"use client";
+'use client';
 
-import * as React from "react";
-import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
-import photo1 from "@/assets/images/photo1.png";
-import Image from "next/image";
-import { Box } from "@/wrappers";
-import { Grid } from "@mui/material";
-import Carousel from "../Carousel";
+import * as React from 'react';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import photo1 from '@/assets/images/photo1.png';
+import photo2 from '@/assets/images/photo2.png';
+import photo3 from '@/assets/images/photo3.png';
+import Image from 'next/image';
+import { Box } from '@/wrappers';
+import { Grid } from '@mui/material';
+import Carousel from '../Carousel';
 
 export default function QuiltedImageList() {
   const itemData = [
@@ -17,12 +19,12 @@ export default function QuiltedImageList() {
       cols: 8,
     },
     {
-      img: photo1,
+      img: photo2,
       rows: 2,
       cols: 4,
     },
     {
-      img: photo1,
+      img: photo3,
       rows: 2,
       cols: 4,
     },
@@ -30,35 +32,24 @@ export default function QuiltedImageList() {
 
   return (
     <>
-      <Grid
-        container
-        item
-        xs={12}
-        sx={{ display: { xs: "none", md: "block" } }}
-      >
-        <ImageList
-          sx={{ width: "100%" }}
-          variant="quilted"
-          cols={12}
-          rowHeight={121}
-        >
+      <Grid container item xs={12} sx={{ display: { xs: 'none', md: 'block' } }}>
+        <ImageList sx={{ width: '100%' }} variant="quilted" cols={12} rowHeight={121}>
           {itemData.map((item) => (
             <ImageListItem
               key={item?.img}
               cols={item?.cols || 1}
               rows={item?.rows || 1}
               sx={{
-                p: "10px",
-              }}
-            >
+                p: '10px',
+              }}>
               <Box
                 component={Image}
                 src={item?.img}
                 sx={{
-                  width: "100%",
-                  height: "100%",
-                  borderRadius: "8px",
-                  objectFit: "cover",
+                  width: '100%',
+                  height: '100%',
+                  borderRadius: '8px',
+                  objectFit: 'cover',
                 }}
                 alt={item?.title}
               />
@@ -67,29 +58,28 @@ export default function QuiltedImageList() {
         </ImageList>
       </Grid>
 
-      <Box sx={{ width: "100vw", display: { xs: "block", md: "none" } }}>
+      <Box sx={{ width: '100vw', display: { xs: 'block', md: 'none' } }}>
         <Carousel
           items={itemData.map((d: any, index: number) => (
             <Box
               key={index}
               sx={{
                 // background: "red",
-                width: "330px",
-                height: { xs: "240px", md: "273px" },
-                position: "relative",
-                overflow: "hidden",
-                borderRadius: "16px",
-              }}
-            >
+                width: '330px',
+                height: { xs: '240px', md: '273px' },
+                position: 'relative',
+                overflow: 'hidden',
+                borderRadius: '16px',
+              }}>
               <Box
                 sx={{
-                  marginLeft: index > 0 ? "20px" : "20px",
-                  width: "100%",
-                  height: "100%",
-                  objectFit: "cover",
-                  objectPosition: "bottom",
-                  position: "absolute",
-                  borderRadius: "16px",
+                  marginLeft: index > 0 ? '20px' : '20px',
+                  width: '100%',
+                  height: '100%',
+                  objectFit: 'cover',
+                  objectPosition: 'bottom',
+                  position: 'absolute',
+                  borderRadius: '16px',
                 }}
                 component={Image}
                 alt="houses and properties for rent"
