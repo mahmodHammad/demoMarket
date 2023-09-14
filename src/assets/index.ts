6a41c05334bda4a5ea498698d6b605c86455c454
Abdulrahman Mamdouh <abdulrahman.mamdouh@meng-tech.com>
2023-09-14 18:01:03 +0300

    add more location button to location accordion

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -26,3 +26,4 @@ export { default as Plus } from "./icons/Plus";
 export { default as Filter } from "./icons/Filter";
 export { default as OutlineHeart } from "./icons/OutlineHeart";
 export { default as FilledHeart } from "./icons/FilledHeart";
+export { default as SearchLine } from "./icons/SearchLine";
