08dd921d6661aa9b9a611c23d7e3995e7984eef1
abawazir <abawazir@goatar.com>
2023-09-17 12:22:36 +0300

    booking details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <Box>
       {/* Logged In component */}
-      <BookingDetails />
+      {/* <BookingDetails /> */}
       {/* <MyBookings/> */}
       {/* <MyBookings/> */}
       {/* <MyPayments/> */}
