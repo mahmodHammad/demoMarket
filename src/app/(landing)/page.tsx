55f6b1524397cce7f937479751c6ec4d8048f112
shreyas-k <shreyaskanzarkar@gmail.com>
2023-09-18 17:24:34 +0530

    landing page restored from main branch

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -3,25 +3,16 @@ import MostViewed from '@/component/pages/homepage/MostViewed';
 import RecentlyAdded from '@/component/pages/homepage/RecentlyAdded';
 import { Box } from '@/wrappers';
 import { Container } from '@mui/material';
-import MyBookings from '../(dash)/my-bookings/page';
 
 export default function Home() {
   return (
     <Box>
-      {/* Logged In component */}
-      {/* <BookingDetails /> */}
-      <MyBookings/>
-      {/* <MyBookings/> */}
-      {/* <MyPayments/> */}
-
-      {/* Logged out component */}
-      {/* <Hero />
       <Hero />
       <Container maxWidth="xl" sx={{ pb: { xs: '90px', md: '164px' } }}>
         <Neighbourhoods />
         <RecentlyAdded />
         <MostViewed />
-      </Container> */}
+      </Container>
     </Box>
   );
 }
