f573cbbefe48b46fb107d004c852db5ba8776b5a
Mahmoud Hammad <hodaman2012@gmail.com>
2023-09-27 22:50:03 +0300

    customize for phone

--- src/component/forms/RadioGroup.tsx
+++ src/component/forms/RadioGroup.tsx
@@ -1,3 +1,5 @@
+import theme from '@/ThemeRegistry/theme';
+import { Text } from '@/wrappers';
 import {
 	RadioGroup as MUIRadioGroup,
 	FormControl,
@@ -5,6 +7,7 @@ import {
 	FormControlLabel,
 	Radio,
 	RadioGroupProps as MUIRadioGroupProps,
+	useMediaQuery,
 } from '@mui/material';
 
 type Option = {
@@ -19,17 +22,24 @@ type RadioGroupProps = MUIRadioGroupProps & {
 
 const RadioGroup = ({ label, options, ...otherProps }: RadioGroupProps) => {
 	// TODO: customize style
+	const isMobileView = useMediaQuery(theme.breakpoints.down('md'));
+
 	return (
 		<FormControl sx={{ width: '100%', alignItems: 'flex-start' }}>
-			<FormLabel sx={{ fontSize: '12px', color: '#525451' }}>
-				{label}
-			</FormLabel>
+			<FormLabel sx={{ fontSize: '12px', color: '#525451' }}>{label}</FormLabel>
 			<MUIRadioGroup
 				row
-				sx={{ justifyContent: 'space-between', width: '100%', padding: '9px' }}
+				sx={{ justifyContent: 'space-between', width: '100%', paddingLeft: '8px' }}
 				aria-label={label}
 				{...otherProps}>
-				{options?.map((option) => <FormControlLabel value={option.value} control={<Radio />} label={option.label} />)}
+				{options?.map((option, index) => (
+					<FormControlLabel
+						key={index}
+						value={option.value}
+						control={<Radio size={isMobileView ? 'small' : 'medium'} />}
+						label={<Text variant="small">{option.label}</Text>}
+					/>
+				))}
 			</MUIRadioGroup>
 		</FormControl>
 	);
