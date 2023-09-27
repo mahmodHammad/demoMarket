1827fa8fe70ac6cdd923667a1003a9026d10af2b
Mahmoud Hammad <hodaman2012@gmail.com>
2023-09-28 01:35:36 +0300

    Refactor layouts, visitor separated from admin

--- src/app/(visitor)/layout.tsx
+++ src/app/(visitor)/layout.tsx
@@ -2,10 +2,10 @@
 
 import * as React from 'react';
 import ThemeRegistry from '@/ThemeRegistry/ThemeRegistry';
-import AppLayout from '@/component/loggedInLayout/AppLayout'; 
+import AppLayout from '@/component/loggedInLayout/AppLayout';
 
 export const metadata = {
-	title: 'Admin dashboard',
+	title: 'Visitor dashboard',
 	description:
 		'Property Portal Explore Our Wide Range of Properties for Sale and Rentals in Saudi arabia, Golf, and middle east and more Verified Listings: Apartments, Villas, Houses Find Your Perfect Appartment their',
 };
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<ThemeRegistry>
-			<AppLayout  >{children}</AppLayout>
+			<AppLayout type="visitor">{children}</AppLayout>
 		</ThemeRegistry>
 	);
 }
--- src/component/loggedInLayout/Drawer.tsx
+++ src/component/loggedInLayout/Drawer.tsx
@@ -1,27 +1,78 @@
-import Sidebar from "./Sidebar";
-import AtarColoredLogo from "@/assets/icons/AtarColoredLogo";
-import { Box } from "@/wrappers/layouts";
+import Sidebar from './Sidebar';
+import AtarColoredLogo from '@/assets/icons/AtarColoredLogo';
+import { Box } from '@/wrappers/layouts';
+import MuiDrawer from '@mui/material/Drawer';
+import { CSSObject, styled, Theme } from '@mui/material/styles';
 
-const DrawerContainer = () => {
-  return (
-    <>
-      <Box sx={{
-        paddingLeft: '24px',
-        marginTop: '24px',
-        marginBottom: '24px',
-        height: "auto",
-        objectFit: "contain",
-        cursor: "pointer",
-      }}>
-        <AtarColoredLogo
-          sx={{
-            height: "52px",
-            width: "133px",
-          }}
-        />
-      </Box>
-      <Sidebar />
-    </>
-  );
+const drawerWidth: number = 240;
+const open = true;
+const DrawerContainer = ({ type }) => {
+	const openedMixin = (theme: Theme): CSSObject => ({
+		width: drawerWidth,
+		border: 'none',
+		transition: theme.transitions.create('width', {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.enteringScreen,
+		}),
+		overflowX: 'hidden',
+		padding: '0 1rem 1rem 2rem',
+	});
+
+	const closedMixin = (theme: Theme): CSSObject => ({
+		transition: theme.transitions.create('width', {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen,
+		}),
+		border: 'none',
+		overflowX: 'hidden',
+		width: `100px`,
+		padding: '2rem',
+	});
+
+	const Drawer = styled(MuiDrawer, {
+		shouldForwardProp: (prop) => prop !== 'open',
+	})(({ theme, open }) => ({
+		width: drawerWidth,
+		flexShrink: 0,
+		whiteSpace: 'nowrap',
+		boxSizing: 'border-box',
+		...(open && {
+			width: drawerWidth,
+			...openedMixin(theme),
+			'& .MuiDrawer-paper': openedMixin(theme),
+		}),
+		...(!open && {
+			width: '100px',
+			...closedMixin(theme),
+			'& .MuiDrawer-paper': closedMixin(theme),
+		}),
+	}));
+
+	return (
+		<Drawer
+			variant="permanent"
+			open={true}
+			sx={{
+				display: { xs: 'none', sm: 'block' },
+			}}>
+			<Box
+				sx={{
+					paddingLeft: '24px',
+					marginTop: '24px',
+					marginBottom: '24px',
+					height: 'auto',
+					objectFit: 'contain',
+					cursor: 'pointer',
+				}}>
+				<AtarColoredLogo
+					sx={{
+						height: '52px',
+						width: '133px',
+					}}
+				/>
+			</Box>
+			<Sidebar type={type} />
+		</Drawer>
+	);
 };
 export default DrawerContainer;
