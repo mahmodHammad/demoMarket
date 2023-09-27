9bfc8ecb7e45f31f835b844cb62142f7587f5f8b
Mahmoud Hammad <hodaman2012@gmail.com>
2023-09-27 23:45:23 +0300

    Fix plusicon color, add unit to market button&page

---
+++ src/app/(dash)/properties/addUnit/page.tsx
@@ -0,0 +1,66 @@
+'use client'
+
+import { Box, Button, Text } from '@/wrappers';
+import { Grid } from '@mui/material';
+import Link from 'next/link';
+
+import neigbourhoodCover from '@/assets/images/neigbourhoodCover.png';
+import neibourhoodcover2 from '@/assets/images/neibourhoodcover2.png';
+// import { UnitsCard } from '@/component';
+// import { Delete } from '@/assets';
+
+import React    from 'react';
+import { UnitsCard } from '@/component'; 
+
+const data = [
+	{
+		title: 'Al-Arid District',
+		img: neibourhoodcover2,
+		link: '/',
+		price: 'SAR 60,000',
+		area: '120 sqm',
+		location: 'Riyadh',
+	},
+	{ title: 'Al-Arid District', img: neibourhoodcover2, link: '/' },
+	{ title: 'Yarmouk Neighbourhood', img: neigbourhoodCover, link: '/' },
+	{ title: 'Yarmouk Neighbourhood', img: neigbourhoodCover, link: '/' },
+	{ title: 'Al-Arid District', img: neibourhoodcover2, link: '/' },
+	{ title: 'Yarmouk Neighbourhood', img: neigbourhoodCover, link: '/' },
+	{ title: 'Al-Arid District', img: neibourhoodcover2, link: '/' },
+	{ title: 'Yarmouk Neighbourhood', img: neigbourhoodCover, link: '/' },
+];
+
+ 
+const Properties = () => {
+	return (
+		<>
+			<Box column p={'35px'} width={'100%'}>
+				<Box center width={'100%'} xbetween row>
+					<Text variant="h4">Properties List</Text>
+					<Box>
+						<Button variant="outlined" component={Link} href="/properties">
+							Select Multiple
+						</Button>
+					</Box>
+				</Box>
+
+				<Grid container mt={'25px'} spacing={'28px'}>
+					{data?.map((d, index) => (
+						<Grid item xs={4} key={index}>
+							<UnitsCard
+								title={d?.title}
+								img={d?.img}
+								link={d?.link}
+								price={d?.price}
+								area={d?.area}
+								location={d?.location}
+							/>
+						</Grid>
+					))}
+				</Grid>
+			</Box>
+		</>
+	);
+};
+export default Properties
+  
--- src/assets/icons/Plus.tsx
+++ src/assets/icons/Plus.tsx
@@ -1,23 +1,12 @@
 import { SvgIcon } from '@mui/material';
 
-export default function Close(props: any) {
+export default function Plus(props: any) {
 	return (
-		<SvgIcon
-			{...props}
-			inheritViewBox
-		>
-			<svg
-				width='24'
-				height='24'
-				viewBox='0 0 24 24'
-				fill='none'
-				xmlns='http://www.w3.org/2000/svg'
-			>
-				<path
-					d='M11 11V5H13V11H19V13H13V19H11V13H5V11H11Z'
-					fill='#4F5154'
-				/>
+		<SvgIcon {...props} inheritViewBox>
+			<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+				<path d="M11 11V5H13V11H19V13H13V19H11V13H5V11H11Z" />
 			</svg>
 		</SvgIcon>
 	);
 }
+
