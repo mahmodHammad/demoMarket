58abafc95c6f0b8dd039f9f4fa95287426ba8af9
Abdulrahman Mamdouh <abdulrahman.mamdouh@meng-tech.com>
2023-10-11 12:58:59 +0300

    add loading effect

--- src/wrappers/index.ts
+++ src/wrappers/index.ts
@@ -2,4 +2,5 @@ export { default as Text } from "./Text";
 export { default as Button } from "./Button";
 export { default as Accordion } from "./Accordion";
 export { default as Chip } from "./Chip";
+export { default as Loading } from "./Loading";
 export { Box, Item, Container } from "./layouts";
