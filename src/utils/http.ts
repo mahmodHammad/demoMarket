f088bd1ee5732e9f0a8204157ae5ea529d215b3c
shreyas-k <shreyaskanzarkar@gmail.com>
2023-10-13 17:37:48 +0530

    profile image api added

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -70,8 +70,6 @@ export const http = axios.create({
 http.interceptors.request.use(
 	(config) => {
 		config.headers['X-Tenant'] = 'testDemoOne';
-		config.headers['Content-Type'] = 'application/json';
-		config.headers['Accept'] = 'application/json';
 		config.headers['Access-Control-Allow-Origin'] = '*';
 		config.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept';
 		return config;
