d4ea7eeea54f0d710f8a813a92b3baf70252cdbe
Abdulrahman Mamdouh <abdulrahman.mamdouh@meng-tech.com>
2023-10-17 18:15:15 +0300

    fix build error

--- src/component/loggedInLayout/ProfileDropDown.tsx
+++ src/component/loggedInLayout/ProfileDropDown.tsx
@@ -68,7 +68,6 @@ const ProfileDropDown = () => {
 	const [open, setOpen] = useState(false);
 	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 	const myRef = React.useRef<HTMLDivElement>(null);
-	const { user } = useAuth();
 
 	const handleClick = (event: React.MouseEvent<HTMLElement>) => {
 		setAnchorEl(event.currentTarget);
