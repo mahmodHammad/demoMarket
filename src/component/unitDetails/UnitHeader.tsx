64dd044cbe02a205ed01b358e3e231ab324a31f5
Mahmoud Hammad <hodaman2012@gmail.com>
2023-10-19 01:52:49 +0200

    handle fav,share in unitdetails,

--- src/component/unitDetails/UnitHeader.tsx
+++ src/component/unitDetails/UnitHeader.tsx
@@ -1,21 +1,25 @@
 'use client';
-import { Favorite, Share } from '@/assets';
+import { Favorite, FilledHeart, OutlineHeart, Share } from '@/assets';
 import { useAuth } from '@/contexts/AuthContext';
 import { Box, Button, Text } from '@/wrappers';
 import { IconButton } from '@mui/material';
-
+import { RWebShare } from 'react-web-share';
 interface Props {
 	logo: any;
 	title: string;
 	location: string;
+	liked?: boolean;
+	id: string;
 }
-export default function UnitHeader({ logo, title, location }: Props) {
+export default function UnitHeader({ id, logo, title, location, liked, handleToggleLike }: Props) {
 	const { isAuthed, openLoginModal } = useAuth();
 
 	const handleButton = () => {
 		if (!isAuthed) {
 			openLoginModal();
 			return;
+		} else {
+			handleToggleLike();
 		}
 	};
 	return (
@@ -33,19 +37,30 @@ export default function UnitHeader({ logo, title, location }: Props) {
 			</Box>
 
 			<Box row ycenter gap="12px">
-				<IconButton
-					color="primary"
-					aria-label="delete"
-					sx={{
-						display: { xs: 'block', md: 'none' },
-						height: '30px',
-						width: '30px',
-						mt: '-8px',
+				<RWebShare
+					sites={['whatsapp']}
+					data={{
+						text: 'Like humans, flamingos make friends for life',
+						url: 'https://on.natgeo.com/2zHaNup',
+						title: 'Flamingos',
 					}}
-					// size="small"
+					// onClick={() => console.log('shared successfully!')}
 				>
-					<Share />
-				</IconButton>
+					<IconButton
+						color="primary"
+						aria-label="delete"
+						sx={{
+							display: { xs: 'block', md: 'none' },
+							height: '30px',
+							width: '30px',
+							mt: '-8px',
+						}}
+						// size="small"
+					>
+						<Share />
+					</IconButton>
+				</RWebShare>
+
 				<IconButton
 					color="primary"
 					sx={{
@@ -57,20 +72,30 @@ export default function UnitHeader({ logo, title, location }: Props) {
 					size="large">
 					<Favorite />
 				</IconButton>
-				<Button
-					variant="outlined"
-					sx={{
-						display: { xs: 'none', md: 'flex' },
-						height: '42px',
-						width: '100%',
-						color: '#002A37',
-						padding: '12px 24px 12px 24px',
-						borderRadius: '8px',
-						borderColor: '#E3E3E3',
+				<RWebShare
+					sites={['copy', 'whatsapp', 'facebook', 'twitter', 'telegram', 'linkedin', 'mail']}
+					data={{
+						text: 'unit details',
+						url: `http://localhost:3000/unitdetails/${id}`,
+						title: 'unit details',
 					}}
-					startIcon={<Share size="small" />}>
-					Share
-				</Button>
+					// onClick={() => console.log('shared successfully!')}
+				>
+					<Button
+						variant="outlined"
+						sx={{
+							display: { xs: 'none', md: 'flex' },
+							height: '42px',
+							width: '100%',
+							color: '#002A37',
+							padding: '12px 24px 12px 24px',
+							borderRadius: '8px',
+							borderColor: '#E3E3E3',
+						}}
+						startIcon={<Share size="small" />}>
+						Share
+					</Button>
+				</RWebShare>
 
 				<Button
 					variant="outlined"
@@ -84,7 +109,9 @@ export default function UnitHeader({ logo, title, location }: Props) {
 						borderRadius: '8px',
 						borderColor: '#E3E3E3',
 					}}
-					startIcon={<Favorite />}>
+					// {liked ? <FilledHeart /> : <OutlineHeart />}
+
+					startIcon={liked ? <FilledHeart /> : <Favorite />}>
 					Favorite
 				</Button>
 			</Box>
