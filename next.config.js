b751f3511b19d0233abb20e56df9841c6b09616c
Mahmoud Hammad <hodaman2012@gmail.com>
2023-11-01 11:13:59 +0200

    sidra mdomian

--- next.config.js
+++ next.config.js
@@ -19,7 +19,7 @@ const nextConfig = {
 				// pathname: '/account123/**',
 			},
 		],
-		domains: ['193.122.88.9', 'localhost','api-dev.hectare.app', 'marketplace.goatar.com','ax0mjhjyzasf.compat.objectstorage.me-jeddah-1.oraclecloud.com'],
+		domains: ['sedra.roshn.sa','193.122.88.9', 'localhost','api-dev.hectare.app', 'marketplace.goatar.com','ax0mjhjyzasf.compat.objectstorage.me-jeddah-1.oraclecloud.com'],
 	},
 };
 
