d3b09aab7860c3dbd2b58f44af5490ccec54a353
Mahmoud Hammad <hodaman2012@gmail.com>
2023-11-08 13:58:53 +0200

    demo extenent

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -7,7 +7,7 @@ type Options = {
 	onError?: (error: Error) => void;
 };
 
-const xTenant = 'roshn';
+const xTenant = 'demo';
 export const get = async (url: string, options: Options = {}) => {
 	// const xTenant = process.env.X_TENANT
 	try {
